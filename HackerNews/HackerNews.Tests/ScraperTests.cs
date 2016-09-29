d4378d712c3c274cef37dacbb1bd091d2e056e07
Alexander Frost <alex@fxfp.com>
2016-09-29 01:05:31 +0100

    A couple more unit tests.

---
+++ HackerNews/HackerNews.Tests/ScraperTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerNews.Tests
+{
+    [TestClass]
+    public class ScraperTests
+    {
+        [TestMethod]
+        public void BadInputTest()
+        {
+            Scraper scr = new Scraper(-1);
+            scr.Run();
+
+            Assert.IsTrue(scr.Results.Count == 0);
+        }
+
+        [TestMethod]
+        public void EmptyTest()
+        {
+            Scraper scr = new Scraper(0);
+            scr.Run();
+
+            Assert.IsTrue(scr.Results.Count == 0);
+        }
+
+        [TestMethod]
+        public void MeaningOfLifeTest()
+        {
+            Scraper scr = new Scraper(42);
+            scr.Run();
+
+            Assert.IsTrue(scr.Results.Count == 42);
+        }
+    }
+}
